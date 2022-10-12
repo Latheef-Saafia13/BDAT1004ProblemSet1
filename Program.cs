78bbbbbd7c48c99b0f70227246270c0f81760104
Latheef-Saafia13 <115661940+Latheef-Saafia13@users.noreply.github.com>
2022-10-12 15:46:28 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Countthestring
+{
+    public static void Main()
+    {
+        String string1 = "Supercalifragilisticexpialidocious";
+        int count = 0;
+
+        for (int i = 0; i < string1.Length; i++)
+        {
+            if (string1[i] != ' ')
+                count++;
+        }
+
+        Console.WriteLine("Total number of characters in a string: " + count);
+    }
+}
